888029e71ceb91832c02fc1efb93cbf69ebcc97a
Rut <rutwintzell@outlook.com>
2026-03-26 17:40:56 +0100

    Started working on index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
